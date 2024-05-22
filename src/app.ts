4c935007b5fb252f92850b41c603c06d06103362
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-05-22 19:48:09 +0600

    Data Management

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import { ProductRoutes } from './modules/products/product.route';
 import { OrderRoutes } from './modules/orders/order.route';
-import notFoundRoute from './notFoundRoute';
+
 const app: Application = express();
 
 // parsers
